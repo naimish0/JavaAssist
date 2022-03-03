800f1c23a2217c1204593a29a43615651af1803c
Naimish Gupta <naimishgupta983842377@gmail.com>
2022-03-03 15:53:47 +0530

    Initail Commit

---
+++ app/src/main/java/com/animation/javaassist/Calculator.java
@@ -0,0 +1,8 @@
+package com.animation.javaassist;
+
+public class Calculator {
+    public String simpleMethod()
+    {
+        return "This is Method in Calculator Class";
+    }
+}
---
+++ app/src/main/java/com/animation/javaassist/MainActivity.java
@@ -0,0 +1,36 @@
+package com.animation.javaassist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import javassist.CannotCompileException;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.CtNewMethod;
+import javassist.NotFoundException;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);                             //Replacing Content of Method at runtime and adding a new Method in Calculator class
+        try {
+            ClassPool classPool = ClassPool.getDefault();
+            CtClass ctClass = classPool.get("com.sample.app.Calculator");
+            CtMethod ctMethod = ctClass.getDeclaredMethod("simpleMethod");
+            ctMethod.setBody("{ return \"This is still a method in Calculator class but we can modify a method here \"; }");
+            CtMethod addnewMethod = CtNewMethod.make("public int newlyMethod(int dx){x+=dx;}", ctClass);
+            ctClass.addMethod(addnewMethod);
+            ctClass.toClass();
+            Calculator calculator = new Calculator();
+            String msg = calculator.simpleMethod();
+            System.out.println(msg);
+        } catch (NotFoundException | CannotCompileException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
